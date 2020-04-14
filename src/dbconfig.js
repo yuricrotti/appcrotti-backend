4543945fdbf56054bcadd53d7b705fba8013ac3d
Yuri <yuricrotti@hotmail.com>
2020-04-14 16:49:26 -0300

    Adiciona Procfile

---
+++ src/dbconfig.js
@@ -0,0 +1,7 @@
+let dados = {
+	url: "mongodb://<crotti>:<ua6ZUYuVamJt@t8>@ds157723.mlab.com:57723/heroku_tcscdhzk",
+	user: "crotti",
+	password: "ua6ZUYuVamJt@t8f",
+}
+
+exports.dados =  dados ; 
