507b35bba951a67db4e5be34bd90bfa8b05dd9a1
Yuri <yuricrotti@hotmail.com>
2020-04-14 16:48:05 -0300

    Adiciona Procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
-const {dados} = require('./src/dbconfig')
+require("dotenv").config();
 
 //Iniciando o app
 const app = express();
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 //iniciando o DB
-console.log(dados.url)
-mongoose.connect(dados.url);
+
+mongoose.connect(process.env.MONGO_URL , { useNewUrlParser: true });
 
 requireDir('./src/models');
 
